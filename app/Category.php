1ee2c45f1ce71f06eef6b1e8e6edb7ba6d430d55
ali <ali.ouchagour01@gmail.com>
2021-05-11 23:34:25 +0100

    soft delete categpories

--- app/Category.php
+++ app/Category.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     //
+    use SoftDeletes;
     public function Posts()
     {
         // nb: hena 3dna hasMany y3niii (1.n) dakchii lach smiina function plurel 
