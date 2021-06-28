1cb8208d5612d7a29411f9fc82d9f9daac5dcc1a
ali <ali.ouchagour01@gmail.com>
2021-06-28 22:11:43 +0100

    to be contuned setting option

---
+++ app/Settings.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Settings extends Model
+{
+    //
+
+    protected $fillable=['site_name','contact_number','contact_email','address'];
+
+
+}
+
