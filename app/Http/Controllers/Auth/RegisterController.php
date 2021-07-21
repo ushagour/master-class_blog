28f59a96523ccf14c00f49ddb8f4ad8a58c3ca53
ushagour <55017548+ushagour@users.noreply.github.com>
2021-07-22 00:40:59 +0100

    creation dyal profile d user men b3d registration 1er fois

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
+use App\Profile;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -64,10 +65,18 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user=User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        $profile = Profile::create([
+        #les autres  champs sont par defult null 
+        'user_id'=>$user->id,
+        'avatar'=>'avatars/defult_user.png'
+        
+        ]);
+
+ return $user;
     }
 }
