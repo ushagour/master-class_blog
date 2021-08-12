35a87ce66e4a0771c95a99f7d9579e13117ad5a9
ushagour <55017548+ushagour@users.noreply.github.com>
2021-08-12 14:56:57 +0100

    mailing todo hta nkml dinmha

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use App\Category;
+use App\Mail\TestMail;
 use App\Post;
 use App\User;
 use App\Tag;
@@ -32,4 +34,26 @@ class HomeController extends Controller
         ->with('count_categories',Category::all()->count())
         ->with('count_users',User::all()->count());
     }
+
+    public function SendMail()
+    {
+         $detail=[
+    'title'=>'test mail by ali',
+    'body' =>'the body of the mail '
+         ];
+         Mail::to("test@test.com")->send(new TestMail($detail));
+
+
+
+//          use Mail;
+
+// Mail::send('mail.login-verificatio', $data, function($message) use($email_to) {
+//     $message->to($email_to, '')->subject
+//         ('Login Security Number');
+//     $message->from('ali.ouchagour01@gmail.com','xxxxxxxxxxx');
+//  });
+return "email seend";
+
+    }
+
 }
